2466c50789aa9dbed1823a72c2d50f509ecbfaf8
Barham Slah <barhamsalahaddin@gmail.com>
2026-08-11 13:43:32 +0300

    Add code for Generics.java

--- Generics.java
+++ Generics.java
@@ -8,3 +8,34 @@ Generics force you to specify what data type a collection or class works with.
 If you try to pass the wrong data type, Java catches the mistake
 while you code (at compile time) rather than crashing your program when it runs (at runtime).
 
+***************************************************************************************
+
+public class Main {
+
+    public static void main() {
+
+        Box<String> box = new Box<>();
+
+        box.setThing("a");  // if you don't write that the compiler will give null
+
+        System.out.println(box.getThing());
+
+    }
+}
+
+//////////////
+public class Box<T>{
+
+    T thing;
+
+    public void setThing(T thing){
+        this.thing = thing;
+    }
+
+    public T getThing(){
+        return this.thing;
+    }
+
+}
+*********************************************************************************************
+
