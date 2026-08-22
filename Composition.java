d8145a6064900f17f35065d000d70b26a1579ea3
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-08-22 18:44:47 +0300

    Add composition concept and rules to Composition.java
    Added explanation of composition and its rules.

--- Composition.java
+++ Composition.java
@@ -1,1 +1,14 @@
+Composition means a big thing owns a smaller thing, and the smaller thing cannot live without the big thing.
 
+It is a "HAS-A" relationship. A House HAS-A Room.
+
+Simple Rules
+The Owner (Big): The House creates and holds the Room.
+
+The Part (Small): The Room stays inside the House.
+
+Life Rule: If you destroy the House, the Room disappears too.
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  
