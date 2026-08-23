cffdaae18ad0279db96f8a9a1e7e7129022f2319
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-08-23 15:43:14 +0300

    Update Composition.java

--- Composition.java
+++ Composition.java
@@ -11,4 +11,67 @@ Life Rule: If you destroy the House, the Room disappears too.
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  
+void main(){
+
+House house1 = new House("Erbil" , 200 , new Room(5 , 4,4,2,1,1));
+house1.location();
+house1.displayDetails();
+
+System.out.println();
+
+House house2 = new House("Koya" , 100 , new Room(2 , 2,1,1,1,1));
+house2.location();
+house2.displayDetails();
+
+}
+
+///////////////
+
+class House {
+
+    int area;
+    String place;
+    Room room ;
+
+House (String place, int area ,  Room room ) {
+    this.area = area;
+    this.place = place;
+    this.room = room;
+}
+
+void location(){
+    System.out.println("This house located in " + this.place + " and " + this.area + "m² ");
+}
+
+void displayDetails(){
+    room.displayRooms();
+}
+
+}
+
+//////////////////
+
+class Room {
+
+    int toilet;
+    int bedroom;
+    int bathroom;
+    int kitchen;
+    int hall;
+    int livingroom;
+
+    Room(int toilet , int bedroom , int bathroom , int kitchen, int hall , int livingroom) {
+        this.livingroom = livingroom;
+        this.hall = hall;
+        this.bedroom = bedroom;
+        this.kitchen = kitchen;
+        this.bathroom = bathroom;
+        this.toilet = toilet;
+    }
+
+void displayRooms(){
+    System.out.println("This house has " + this.bathroom + " Bathroom & " + this.kitchen + " Kitchen & " + this.hall + " Hall & " +  this.livingroom +
+            " Livingroom & " + this.bedroom + " Bedroom and " + this.toilet + " Toilet" );
+}
+
+} 
