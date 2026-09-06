7b9c4d1f6794950bde23bc4ca5b6664791b5fa78
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-06 17:20:55 +0300

    Create StaticBlock.java

---
+++ StaticBlock.java
@@ -0,0 +1,13 @@
+Syntax: You simply write static { ... } with curly braces inside the class.
+
+Timing: It runs as the very first thing when the class is loaded into memory by Java.
+
+Frequency: It runs only once per program execution.
+
+No triggers needed: You do not need to create an object with new, nor do you need to call it like a method. Java triggers it automatically.
+
+Write one static block: Even though Java allows multiple static blocks,( writing one is best practice) . It keeps your code clean, organized, and easy to read.
+
+Class level only: Static blocks can only be declared directly inside the class body—never inside a method.
+
+  
