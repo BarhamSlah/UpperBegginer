3f31772c7057f283a4168c04201a4d23bdab1e3e
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-07 21:48:01 +0300

    Enhance comments for Buggati constructor clarity
    Added comments explaining constructor behavior in Java.

--- OverloadingConstructor.java
+++ OverloadingConstructor.java
@@ -22,13 +22,17 @@ public class Buggati extends Car {
     Buggati(int n) {
         super(n);
         System.out.println("Parameter buggati constructor");
-
+        
+// If you want to call the default parent constructor, you don't need to write super() because Java inserts it automatically. 
+// However, if you want to call a parameterized parent constructor, you must explicitly write super(...) and pass an argument.
     }
 }
 
 
 void main()  {
 
-    new Buggati();
-
+new Buggati();     // prints: Parameterized Car Constructor \n Default Buggati Constructor
+// new Buggati(5); // prints: Parameterized Car Constructor \n Parameterized Buggati Constructor
+    
+    
     }
