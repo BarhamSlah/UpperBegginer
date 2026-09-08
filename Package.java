5c5f11760356e7341508e2cca6d55439f2791f56
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-08 18:08:07 +0300

    Add Java package documentation
    Added documentation on Java package usage and conventions.

---
+++ Package.java
@@ -0,0 +1,17 @@
+In Java package : is a mechanism used to group related (classes, interfaces, and sub-packages) together acting like a folder structure on your laptop.
+
+To declare a package :  use the package keyword at the very first line of your Java source file.
+
+Naming Convention: Package names are written in all lowercase to avoid conflicts with class names.
+
+Automatic Import: The java.lang package (which contains fundamental classes like String, System, and Math) 
+is automatically imported into every Java program without needing an explicit import statement.
+
+import vehicles.Car;
+means:
+"Java, I want to use the Car class from the vehicles package."  
+
+Package = Folder 📁
+Class = File 📄
+
+  
