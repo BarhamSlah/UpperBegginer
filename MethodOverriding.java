d28a60ca0d976052864fc48c84b267edba64425a
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-08 12:20:21 +0300

    Update MethodOverriding.java

--- MethodOverriding.java
+++ MethodOverriding.java
@@ -9,6 +9,8 @@ Action/Task: Different (modified in subclass)
 Method Override: when you write a method in SubClass that already has in SuperClass but there is a different that is the task in the method
 but you should inheritance it and same name of method and should be same datatypes
 
+Method Overriding requires two classes—a Superclass (parent) and a Subclass (child) . Since there is no inheritance here, it cannot be overriding
+
 that is not necessarily but good if you do it :
 write @methodoverriding before the method that you wanna overriding it
 
