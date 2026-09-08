a0a179abb54a2457ad71888588e09530a05191f3
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-08 12:18:09 +0300

    Update MethodOverloading.java

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,9 +1,26 @@
-Method Overloading :
-Method Name: Must be same
-Parameter Data Types: Must be different
-Return Data Type: Can be same or different
-Action/Task: Different
+It should be different one of them : 
+1- Number of parameter.
+2- data type of parameter.
+3- sequence of parameter.
+This is method overloading when they have the same name of method.  
 
 *********************************************************************************************************************************************
 
+void main()  {
+
+IO.println(numbers( 2  , 3  , 4));
+
+    }
+
+public static int numbers(int number1 , int number2){
+    return number1 + number2;
+}
+
+public static int numbers(int number1 , int number2 , int number3){
+return number1 + number2 +  number3;
+}
+
+public static int numbers(int number1 , int number2 , int number3 , int number4){
+return number1 + number2 + number3 + number4;
+}  
   
