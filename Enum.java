aae97a47064fdb92a567126b741485e37064d4b1
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-20 16:19:39 +0300

    Enhance Enum.java with documentation and examples
    Added detailed documentation about enums in Java, including usage examples and method descriptions.

--- Enum.java
+++ Enum.java
@@ -1,1 +1,49 @@
-// Enum : is short for enumeration & special class & use with switch statement
+Enum (Enumeration) is a special Java type used to represent a fixed set of constants.
+Instead of using strings or numbers for values that have limited choices, you can use an enum.
+
+✅ Use enum when:
+You have a fixed set of related values.
+
+An enum is similar to a class because it can have fields, methods, and constructors. However, an enum cannot extend another class or be extended by another class.
+It can implement one or more interfaces.
+
+***********************************************************************************************************************************************************
+  
+ enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+
+ void main() {
+
+    Day today = Day.MONDAY;
+
+    IO.println(today);
+ }
+
+
+/*
+Day → Enum type
+today → Variable
+Day.MONDAY → Enum constant
+*/
+
+Method and Purpose : 
+name()
+Returns the constant's name
+ordinal()
+Returns its position, starting from 0
+values()
+Returns all enum constants
+valueOf()
+Finds a constant by its exact name
+
+ ***********************************************************************************************************************************************************
+
+  
